eb9de0615459819b9a355bd1ed2a71195e19ab60
AcilRestu12 <arieskarestu214@gmail.com>
2021-09-23 00:40:10 +0700

    Fix a important problem

--- App/Main.py
+++ App/Main.py
@@ -11,10 +11,8 @@ client = commands.Bot(command_prefix="-m", Intents = discord.Intents.all())
 for i in range(len(cogs)):
     cogs[i].setup(client)
 
-
-
-
-client.run("ODkwMjQ2NDAyMDY2NzYzODA2.YUtAkg.hH70ZrzEK7CuGG-Kf0ioyM9vau4")
+# Ur
+# client.run("")
 
 
 
